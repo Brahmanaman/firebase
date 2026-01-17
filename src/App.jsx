251cb0da2f91f7dc8489da3e59956aa7f3b193ec
Aman Sharma <sharma9917375201@gmail.com>
2026-01-17 23:41:41 +0530

    learning firebase

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { useFireBaseContext } from "./context/FireBaseContext";
+
+const App = () => {
+  const [userData, setUserData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setUserData({
+      ...userData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const userSignUp = (e) => {
+    e.preventDefault();
+    console.log(userData);
+  };
+
+  return (
+    <>
+      <div className="w-screen h-screen bg-gray-900 flex items-center justify-center">
+        <form
+          className="flex flex-col border rounded-sm w-95 text-white p-3"
+          onSubmit={(e) => userSignUp(e)}
+        >
+          <h1 className="text-center text-2xl">FireBase</h1>
+          <div className="justify-center">
+            <div className="flex flex-col ">
+              <label htmlFor="username">UserName</label>
+              <input
+                className="border rounded-sm py-1 px-2 bg-transparent m-2 ml-0 outline-0"
+                required
+                type="text"
+                name="username"
+                placeholder="enter your username"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="email">Email</label>
+              <input
+                className="border rounded-sm py-1 px-2 bg-transparent m-2 ml-0 outline-none"
+                required
+                type="email"
+                name="email"
+                placeholder="enter your email"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex flex-col ">
+              <label htmlFor="password">Password</label>
+              <input
+                className="border rounded-sm py-1 px-2 bg-transparent m-2 ml-0 outline-none"
+                required
+                type="password"
+                name="password"
+                placeholder="enter your password"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex justify-center">
+              <button
+                type="submit"
+                className="bg-blue-900 py-2 px-4 w-20 rounded-md cursor-pointer active:scale-95"
+              >
+                SignUp
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/context/FireBaseContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useContext } from "react";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set } from "firebase/database";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCVHrfHxeKdkDqh9cz_aHIkfOqvfIPd_FY",
+  authDomain: "fir-64a8a.firebaseapp.com",
+  databaseURL: "https://fir-64a8a-default-rtdb.firebaseio.com",
+  projectId: "fir-64a8a",
+  storageBucket: "fir-64a8a.firebasestorage.app",
+  messagingSenderId: "331393900351",
+  appId: "1:331393900351:web:1a57ad5711f1031a493d8a",
+  measurementId: "G-C4RVFTLTFP",
+  databaseURL: "https://fir-64a8a-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(firebaseConfig);
+const firebaseAuth = getAuth(app);
+const db = getDatabase(app);
+
+const FirebaseContext = createContext(null);
+
+export const useFireBaseContext = () => {
+  return useContext(FirebaseContext);
+};
+
+const FireBaseContext = ({ children }) => {
+  const signUpUserWithEmailAndPassword = async (username, email, password) => {
+    return createUserWithEmailAndPassword(firebaseAuth, email, password);
+  };
+
+  const putData = (key, data) => {
+    set(ref(db, "users/" + key), data);
+  };
+
+  return (
+    <FirebaseContext.Provider
+      value={{ signUpUserWithEmailAndPassword, putData }}
+    >
+      {children}
+    </FirebaseContext.Provider>
+  );
+};
+
+export default FireBaseContext;
